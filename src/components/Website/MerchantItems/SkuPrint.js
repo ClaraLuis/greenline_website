42d047ab9d4a60d56c9d67f2dd4d4a455be7262e
steveroseik <steveroseik@aucegypt.edu>
2024-12-22 11:41:24 +0200

    trial to fix sku print

--- src/components/Website/MerchantItems/SkuPrint.js
+++ src/components/Website/MerchantItems/SkuPrint.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import ReactToPrint from 'react-to-print';
+import ReactToPrint, { useReactToPrint } from 'react-to-print';
 import Sku from './Sku';
 import './Waybill.css';
 import generalstyles from '../Generalfiles/CSS_GENERAL/general.module.css';
@@ -7,20 +7,37 @@ import generalstyles from '../Generalfiles/CSS_GENERAL/general.module.css';
 const SkuPrint = ({ skus }) => {
     const componentRef = useRef();
 
+    const handlePrint = useReactToPrint({
+        pageStyle: '@page { size: 5cm 3cm; margin: 0; }',
+        content: () => componentRef.current,
+        documentTitle: 'SKU Print',
+    });
+
     return (
         <>
-            <ReactToPrint
-                trigger={() => (
-                    <button style={{ height: '35px' }} className={`${generalstyles.roundbutton}`}>
-                        Print SKUs
-                    </button>
-                )}
-                content={() => componentRef.current}
-            />
-            <div class="d-none">
-                <div ref={componentRef} className="print-container ">
+            <button style={{ height: '35px' }} className={`${generalstyles.roundbutton}`} onClick={handlePrint}>
+                Print SKUs
+            </button>
+
+            {/* Hidden Print Content */}
+            <div className="d-none">
+                <div ref={componentRef} className="print-container">
                     {skus.map((item, index) => (
-                        <div key={index} className="print-page">
+                        <div
+                            key={index}
+                            className="print-page"
+                            style={{
+                                height: '3cm',
+                                width: '5cm',
+                                pageBreakAfter: 'always', // Ensures each SKU starts on a new page
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                border: '1px solid black',
+                                padding: '10px',
+                                boxSizing: 'border-box',
+                            }}
+                        >
                             <Sku item={item?.item} />
                         </div>
                     ))}
